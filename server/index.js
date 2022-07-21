6472887f440d52dca206716d60c6deb081f16b3c
Bryce Billingsley <babster24@gmail.com>
2022-07-21 14:31:22 -0500

    added rollbar

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "49a49ba4c6c941b997cb5535e9da4b2d",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
